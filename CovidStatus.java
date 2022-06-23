6bca4da770b91a41e725a12e59e9ea595e1a561e
nabillaauly <101704002+nabillaauly@users.noreply.github.com>
2022-06-23 21:33:43 +0700

    first commit

---
+++ CovidStatus.java
@@ -0,0 +1,40 @@
+package CovidStatus;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CovidStatus {
+
+        public class ArrayListModel {
+            public static class QuickSort {
+               public QuickSort(int)data){
+                this.data=data;
+                    System.out.println("ascending");
+                    System.out.println("Descending");
+                    System.out.println("pilih");
+                    int pilih = in.nextInt();
+
+                public CovidStatus(){
+                }
+
+                public String geData() {
+                    return this.data;
+
+                public int getPilih() {return  this.data}
+                public void setData(String data) {
+                    this.data = geData;
+                }
+            }
+
+            public static void main(String[] args) {
+                ArrayList<CovidStatus> covidStatusArrayList = new ArrayList<>();
+                CovidStatus covid = new covid();
+                covid.setdata;
+                covid.setpilih;
+                covid.add(CovidStatus);
+
+            }
+        }
+
+    }
+}
